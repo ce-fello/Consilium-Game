8fa8f9f7e5877de747f25f27d5bc4f7f8716f201
vsevolodGL <glukhikh.vsevolod@gmail.com>
2024-06-22 02:52:40 +0300

    Reworked some new Images and class stats_gui.py

--- source/gui/stats_gui.py
+++ source/gui/stats_gui.py
@@ -12,6 +12,7 @@ class StatsGUI:
         self.player = player
         self.standard = StandardFunctions(self.root)
         self.show_stability()
+        self.show_budget()
 
     def show_stability(self):
         stability_photo = PhotoImage(file='../resources/images/scales_icon.png')
@@ -26,8 +27,27 @@ class StatsGUI:
                                   window=self.standard.create_nice_no_hover_button("", None, first_photo, 80, 40))
         self.canvas.create_window(220, 10, anchor="nw",
                                   window=self.standard.create_nice_no_hover_button("", None, second_photo, 80, 40))
-        self.canvas.create_window(263, 10, anchor="nw",
+        self.canvas.create_window(260, 10, anchor="nw",
                                   window=self.standard.create_nice_no_hover_button("", None, third_photo, 80, 40))
+
+    def show_budget(self):
+        stability_photo = PhotoImage(file='../resources/images/budget_icon.png')
+        budget = 0
+        for i in self.player.economics.sectors_name:
+            budget += self.player.economics.sectors[i].value
+        budget_photos = []
+        for i in str(budget):
+            if i != '.':
+                budget_photos.append(PhotoImage(file=f'../resources/images/numbers/{i}.png'))
+            else:
+                budget_photos.append(PhotoImage(file=f'../resources/images/numbers/dot.png'))
+
+        self.canvas.create_window(330, 10, anchor="nw",
+                                  window=self.standard.create_nice_no_hover_button("", None, stability_photo, 80,
+                                                                                   80))
+        for i in range(len(budget_photos)):
+            self.canvas.create_window(410 + 40 * i, 10, anchor="nw",
+                                      window=self.standard.create_nice_no_hover_button("", None, budget_photos[i], 80, 40))
 
 
 
